b3d381c6707886049c9990dd34cb4a14c89ff32d
Eshin Ang <eang16@uw.edu>
2018-03-06 15:21:44 -0800

    add animation on button

--- src/index.js
+++ src/index.js
@@ -51,6 +51,7 @@ document.querySelector("#input-form")
         ingreInput.value = "";
         if (searchSpec.ingredients.size >= 3) {
             QUIZ_SUBMIT.disabled = false;   
+            QUIZ_SUBMIT.classList.add("animated", "bounce", "infinite");
         } 
     });
 
@@ -64,6 +65,7 @@ function deleteIngre(ingreid) {
     
     if (searchSpec.ingredients.size < 3) {
         QUIZ_SUBMIT.disabled = true;
+        QUIZ_SUBMIT.classList.remove("animated", "bounce", "infinite");
         document.querySelector("#submit-link").setAttribute("href", "#");
         
     } 
