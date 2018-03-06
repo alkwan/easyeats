232434ade18cb03b3c6ef601344fe037ceae3928
Maggie Tsang <mbytsang@gmail.com>
2018-03-05 22:35:14 -0800

    split html and js into two files, able to fetch from second view

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//Gets all the user input about the recipe they are looking for
+
 "use strict";
 
 let searchSpec = {
@@ -50,7 +52,6 @@ document.querySelector("#add-button")
         searchSpec.currentIngre = ingre;
         render(searchSpec);
         ingreInput.value = "";
-        console.log(searchSpec.ingredients)
     });
 
 function deleteIngre(ingreid) {
@@ -97,162 +98,30 @@ for (let i = 0; i < DIET_CHECK.length; i++) {
     });
 }
 
-
-/////////////////////////////////////////////////////////////////////////////////////
-// Get from API
-
-const API_ID = "135ece56";
-const API_KEY = "461b47c5925be6bbc2ec70ad608f7084";
-const API_RECIPE_URL = "http://api.yummly.com/v1/api/recipes?_app_id=" + API_ID + "&_app_key=" + API_KEY;
-const META_ALLERGY = "http://api.yummly.com/v1/api/metadata/allergy?_app_id=" + API_ID + "&_app_key=" + API_KEY;
-const META_DIET = "http://api.yummly.com/v1/api/metadata/diet?_app_id=" + API_ID + "&_app_key=" + API_KEY;
-
-/* KEEP THIS CHUNK FOR NOW!!!
-let searchSpec = {
-    q:["onion", "bread", "fish"], //&q=onion+bread+fish
-    allergy: ["Dairy-Free", "Gluten-Free"], //meta data = http://api.yummly.com/v1/api/metadata/allergy?_app_id=YOUR_ID&_app_key=YOUR_APP_KEY
-                        //format = &allowedAllergy[]=396^Dairy-Free&allowedAllergy[]=393^Gluten-Free
-    diet: ["Pescetarian", "Lacto vegetarian"] //meta data = http://api.yummly.com/v1/api/metadata/diet?_app_id=YOUR_ID&_app_key=YOUR_APP_KEY
-                        //format = &allowedDiet[]=390^Pescetarian&allowedDiet[]=388^Lacto vegetarian
-} 
-*/
-
-let resultSearch = {
-    ingredients: [],
-    allergy: [],
-    diet: [],
-}
-
-let metaData = {};
-
-//Creates a URL endpoint for the API to grab data
-function createSearchURL() {
-    //Filter search
-    let search = "";
-    let ingreds = resultSearch.ingredients;
-    if (ingreds.length > 0) {
-        search = "&q=";
-        for (let i = 0; i < ingreds.length; i++) {
-            if (i === 0) {
-                search = search + ingreds[i];
-            } else {
-                search = search + "+" + ingreds[i];
-            }
-        }
+document.querySelector("#modal-submit").addEventListener("click", function() {
+    if (searchSpec.ingredients.size >= 3) {
+        //Dont let user submit button until at least 3 ingreds are put in!
+        document.querySelector("#submit-link").setAttribute("href", "recipe.html?" + urlWrangling());
+    } else {
+        window.alert("needs at least 3 ingred!") //MAKE THIS PRETTIER
     }
+});
 
-    //Get allergy data
-    let allergySearch = "";
-    let allergies = resultSearch.allergy;   
-
-    for (let i = 0; i < allergies.length; i++) {
-        for (let j = 0; j < metaData.allergy.length; j++) {
-            if (allergies[i] === metaData.allergy[j].shortDescription) {
-                allergySearch = allergySearch + "&allowedAllergy[]=" + metaData.allergy[j].id + "^" + allergies[i];
-            }
-        }
-    }
+function urlWrangling() {
+    let allergyArr = Array.from(searchSpec.allergy);
+    let ingredArr = Array.from(searchSpec.ingredients);
+    let dietValue = searchSpec.diet;
     
-    //Get diet data
-    let restriction = "";
-    let restrictResults = searchSpec.diet;
-    //console.log(metaData.diet) UI needs Lacto-ovo vegetarian and Paleo, remove just vegetarian
+    let urlParams = new URLSearchParams(window.location.search);
 
-    if (restrictResults && restrictResults !== "") {
-        for (let i = 0; i < metaData.diet.length; i++) {
-            if (metaData.diet[i].shortDescription === restrictResults) {
-                restriction = "&allowedDiet[]=" + metaData.diet[i].id + "^" + restrictResults;
-            }
-        }
+    for (let i = 0; i < allergyArr.length; i++) {
+        urlParams.append("allergy", allergyArr[i]);
     }
-
-    let endpoint = API_RECIPE_URL + search + allergySearch + restriction;
-    console.log(endpoint);
-    return endpoint;
-}
-
-/**
- * Handles responses from the fetch() API.
- * @param {Response} response 
- */
-function handleResponse(response) {
-    if (response.ok) {
-        let results = response.json();
-        console.log(results);
-        return results;
-    } else {
-        return response.json()
-            .then(function(err) {
-                throw new Error(err.errorMessage);
-            });
+    for (let i = 0; i < ingredArr.length; i++) {
+        urlParams.append("ingred", ingredArr[i]);
     }
-}
-
-/**
- * Handles responses from the fetch() API for the meta Data codes.
- * @param {Response} response 
- */
-function parseMetaData(response) {
-    if (response.ok) {
-        return response.text();
-    } else {
-        return response.json()
-            .then(function(err) {
-                throw new Error(err.errorMessage);
-            });
+    if (dietValue && dietValue !== "") {
+        urlParams.append("diet", dietValue);
     }
-}
-
-/**
- * Handles errors that occur while fetching
- * @param {Error} err 
- */
-function handleError(err) {
-    console.error(err.message);
-}
-
-//Yummly API is silly and returns JSONP instead of JSON
-function set_metadata(key, value) {
-    metaData[key] = value;
-}
-
-function getMetaCode(type, codes, resultArr) {
-    for (let i = 0; i < resultArr.length; i++) {
-        for (let j = 0; j < codes.length; j++) {
-            if (resultArr[i] === codes[j].shortDescription) {
-                if (type === "allergy") {
-                    metaData.allergy.push(codes[j].id);
-                } else if (type === "diet") {
-                    metaData.diet.push(codes[j].id);
-                }
-            }
-        }
-    }
-}
-
-
-function onSubmitQuiz() {
-    resultSearch.allergy = Array.from(searchSpec.allergy);
-    resultSearch.ingredients = Array.from(searchSpec.ingredients);
-    resultSearch.diet.push(searchSpec.diet);
-
-    //Handles Metadata URL codes
-    fetch(META_ALLERGY)
-        .then(parseMetaData)
-        .then(rawJs => eval(rawJs))
-        .catch(handleError);
-
-    fetch(META_DIET)
-        .then(parseMetaData)
-        .then(rawJs => eval(rawJs))
-        .catch(handleError);
-        
-    //Cannot get codes right away when calling API
-    setTimeout(function() {
-        getMetaCode("allergy", metaData.allergy, resultSearch.allergy);
-        getMetaCode("diet", metaData.diet, resultSearch.diet);
-        fetch(createSearchURL())
-            .then(handleResponse)
-            .catch(handleError);    //Array results in PromiseValue.matches
-    }, 1000);
+    return urlParams.toString();
 }
